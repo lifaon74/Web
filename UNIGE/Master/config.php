97c9658d881cdb150b79b3e67b2cd1c9fc19d982
lifaon74 <YOUR EMAIL ADDRESS>
2015-04-07 12:01:59 +0200

    last commit

---
+++ UNIGE/Master/config.php
@@ -0,0 +1,28 @@
+<?php
+
+// my code is open source, so for security reason, my passwords are hidden in this file
+// comment this line to use ThingBook
+require_once(__DIR__ . "/private-config.php");
+
+/**
+
+$_CONST_ROOT_PATH				= '/home/user/.../project';							// the absolute path to the root of your project
+		
+$_CONST_MYSQL_HOST				= '127.0.0.1';										// url of your mySQL database
+$_CONST_MYSQL_DB_NAME 			= 'thingbook';										// name of your mySQL database
+$_CONST_MYSQL_USER_NAME 		= 'user';											// user name
+$_CONST_MYSQL_PASSWORD			= 'password';										// password
+		
+
+$_CONST_DYNAMODB_ACCESS_ID		= 'ABCDEFGHIJ123456789';							// access id of your dynamoDB database
+$_CONST_DYNAMODB_ACCESS_KEY		= '123456789ABCDEFGHIJ';							// private key of your dynamoDB database
+$_CONST_DYNAMODB_REGION			= 'eu-west-1';										// region of your dynamoDB database
+
+$_CONST_GRAPHENEDB_HOST_NAME	= 'url.graphenedb.com';								// url of your grapheneDB database
+$_CONST_GRAPHENEDB_HOST_PORT	= 1234												// port of your grapheneDB database
+$_CONST_GRAPHENEDB_DB_NAME		= 'thingbook';										// name of your grapheneDB database
+$_CONST_GRAPHENEDB_KEY			= '123456789ABCDEFGHIJ';							// private key of your grapheneDB database
+
+**/
+
+?>
